8b98ff40f72be58f2d51bcd351c4cc708247d67f
Ephelis Fesia <108088348+Ephelis@users.noreply.github.com>
2025-06-16 18:02:06 +0200

    Add initial skeleton for Minecraft mod

---
+++ src/main/java/com/example/mode/ExampleMod.java
@@ -0,0 +1,12 @@
+package com.example.mode;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(ExampleMod.MODID)
+public class ExampleMod {
+    public static final String MODID = "mode";
+
+    public ExampleMod() {
+        // Register mod setup code here
+    }
+}
